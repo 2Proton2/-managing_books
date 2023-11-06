24ee14ffac908da8e02207ccea616bca34bcffec
2Proton2 <apurva22kalvade@gmail.com>
2023-11-06 19:07:15 +0530

    added mongoDB closing logic

--- index.js
+++ index.js
@@ -4,11 +4,12 @@ const body_parser = require('body-parser');
 const dotenv = require('dotenv');
 dotenv.config({ path: './config.env' })
 const PORT = process.env.PORT;
+const mongoose = require('mongoose');
 
 /**
  * database config
  */
-require('./config/mongodb.connection');
+const database = require('./config/mongodb.connection');
 
 /**
  * use json as packet format
@@ -26,6 +27,36 @@ app.use('/book', require('./router/book.route'));
 /**
  * Hey buddy! I am listenning at @PORT
  */
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
     console.log(`Serving listening at port ${PORT}`);
 })
+
+/**
+ * close the database connection when terminating the program
+ */
+process.on('SIGINT', () => {
+    server.close(() => {
+        mongoose.connection.close()
+        .then(() => {
+            console.log('connection closed');
+            process.exit(0);
+        })
+        .catch((err) => {
+            console.log(`error : ${err}`)
+        })
+        console.log('clsoee');
+    });
+});
+
+process.on('SIGTERM', () => {
+    server.close(() => {
+        mongoose.connection.close()
+        .then(() => {
+            console.log('connection closed');
+            process.exit(0);
+        })
+        .catch((err) => {
+            console.log(`error : ${err}`)
+        });
+    });
+});
