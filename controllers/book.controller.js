5557a8ce2014eaf7862be5082c6a2397da59048b
2Proton2 <apurva22kalvade@gmail.com>
2023-11-06 16:06:58 +0530

    added routes and controller templates

---
+++ controllers/book.controller.js
@@ -0,0 +1,27 @@
+async function addBook(){
+
+}
+
+async function findSpecificBook(){
+
+}
+
+async function findAllBook(){
+
+}
+
+async function updateOneBook(){
+
+}
+
+async function deleteOneBook(){
+
+}
+
+module.exports = {
+    addBook,
+    findSpecificBook,
+    findAllBook,
+    updateOneBook,
+    deleteOneBook
+};
--- router/book.route.js
+++ router/book.route.js
@@ -1,5 +1,12 @@
 const $database_service = require('../config/mongodb.connection');
 const express = require('express');
 const router = express.Router();
+const bookController = require('../controllers/book.controller')
+
+router.post('/add-book', bookController.addBook);
+router.get('/getAll', bookController.findAllBook);
+router.get('/get/:id', bookController.findSpecificBook);
+router.patch('/update/:id', bookController.updateOneBook);
+router.post('/delete/:id', bookController.deleteOneBook)
 
 module.exports = router;
